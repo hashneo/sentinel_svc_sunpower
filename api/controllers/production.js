d4b5288127710f49c9b4ea5e9f389de32415557c
Steven Taylor <steven.taylor@me.com>
2017-05-19 09:25:45 -0700

    Cleanup queries

--- api/controllers/production.js
+++ api/controllers/production.js
@@ -6,26 +6,26 @@ module.exports.getProduction = (req, res) => {
 
     let id = req.swagger.params.id.value;
     let range = req.swagger.params.range.value;
-    let end = req.swagger.params.end.value;
+    let start = req.swagger.params.start.value;
 
-    if ( end ){
-        end = start.toUTCString().replace('GMT','');
-        end = new Date( end );
+    if ( start ){
+        start = start.toUTCString().replace('GMT','');
+        start = new Date( start );
     }
 
-    if ( !end )
-        end = new Date();
+    if ( !start )
+        start = new Date();
 
-    getProductionBy[range]( id, end, res );
+    getProductionBy[range]( id, start, res );
 
 };
 
 let getProductionBy = {};
 
-getProductionBy['hour'] = (id, end, res) => {
+getProductionBy['now'] = (id, start, res) => {
 
-    let ts1 = moment(end).format('YYYY-MM-DD');
-    let ts2 = moment(end).add(1, 'd').format('YYYY-MM-DD');
+    let ts1 = moment(start).add(1, 'd').format('YYYY-MM-DD');
+    let ts2 = moment(start).format('YYYY-MM-DD');
 
     let q = `
     select 
@@ -33,23 +33,22 @@ getProductionBy['hour'] = (id, end, res) => {
         avg(generating) as kwh 
     from 
         sentinel.sunpower_samples 
-    where id = ${id} and timestamp >= ' 00:00:00' and timestamp < '${ts2} 00:00:00' 
+    where id = ${id} and timestamp >= '${ts1} 00:00:00' and timestamp < '${ts2} 00:00:00' 
     group by hour`;
 
     global.module.getData(q)
         .then( (data) => {
-            let r = { start : ts1, end : ts2, samples : data };
-            res.json( { data: r, result : 'ok'  } );
+            res.json( { data: data, result : 'ok'  } );
         })
         .catch( (err) => {
             res.status(500).json( { code: err.code || 0, message: err.message } );
         });
 };
 
-getProductionBy['day'] = (id, end, res) => {
+getProductionBy['day'] = (id, start, res) => {
 
-    let ts1 = moment(end).subtract(1, 'd').format('YYYY-MM-DD');
-    let ts2 = moment(end).format('YYYY-MM-DD');
+    let ts1 = moment(start).format('YYYY-MM-DD');
+    let ts2 = moment(start).add(1, 'd').format('YYYY-MM-DD');
 
     let q = `
     select left( t1.d, 10) as date,
@@ -67,48 +66,17 @@ getProductionBy['day'] = (id, end, res) => {
 
     global.module.getData(q)
         .then( (data) => {
-            let r = { start : ts1, end : ts2, samples : data };
-            res.json( { data: r, result : 'ok'  } );
+            res.json( { data: data, result : 'ok'  } );
         })
         .catch( (err) => {
             res.status(500).json( { code: err.code || 0, message: err.message } );
         });
 };
 
-getProductionBy['week'] = (id, end, res) => {
+getProductionBy['week'] = (id, start, res) => {
 
-    let ts1 = moment(end).subtract(1, 'w').format('YYYY-MM-DD');
-    let ts2 = moment(end).format('YYYY-MM-DD');
-
-    let q = `
-    select left( t1.d, 10) as date,
-           sum(t1.kwh) as kwh
-    from (
-        select 
-            DATE_FORMAT(timestamp, "%Y-%m-%d %H") as d, 
-            avg(generating) as kwh 
-        from 
-            sentinel.sunpower_samples 
-        where id = ${id} and timestamp >= '${ts1} 00:00:00' and timestamp < '${ts2} 00:00:00' 
-        group by d
-    ) as t1
-    group by date`;
-
-
-    global.module.getData(q)
-        .then( (data) => {
-            let r = { start : ts1, end : ts2, samples : data };
-            res.json( { data: r, result : 'ok'  } );
-        })
-        .catch( (err) => {
-            res.status(500).json( { code: err.code || 0, message: err.message } );
-        });
-};
-
-getProductionBy['month'] = (id, end, res) => {
-
-    let ts1 = moment(end).subtract(1, 'm').format('YYYY-MM-DD');
-    let ts2 = moment(end).format('YYYY-MM-DD');
+    let ts1 = moment(start).format('YYYY-MM-DD');
+    let ts2 = moment(start).add(1, 'w').format('YYYY-MM-DD');
 
     let q = `
     select left( t1.d, 10) as date,
@@ -127,18 +95,46 @@ getProductionBy['month'] = (id, end, res) => {
 
     global.module.getData(q)
         .then( (data) => {
-            let r = { start : ts1, end : ts2, samples : data };
-            res.json( { data: r, result : 'ok'  } );
+            res.json( { data: data, result : 'ok'  } );
         })
         .catch( (err) => {
             res.status(500).json( { code: err.code || 0, message: err.message } );
         });
 };
 
-getProductionBy['year'] = (id, end, res) => {
+getProductionBy['month'] = (id, start, res) => {
 
-    let ts1 = moment(end).subtract(1, 'y').format('YYYY-MM-DD');
-    let ts2 = moment(end).format('YYYY-MM-DD');
+    let ts1 = moment(start).format('YYYY-MM-DD');
+    let ts2 = moment(start).add(1, 'M').format('YYYY-MM-DD');
+
+    let q = `
+    select left( t1.d, 10) as date,
+           sum(t1.kwh) as kwh
+    from (
+        select 
+            DATE_FORMAT(timestamp, "%Y-%m-%d %H") as d, 
+            avg(generating) as kwh 
+        from 
+            sentinel.sunpower_samples 
+        where id = ${id} and timestamp >= '${ts1} 00:00:00' and timestamp < '${ts2} 00:00:00' 
+        group by d
+    ) as t1
+    group by date`;
+
+
+    global.module.getData(q)
+        .then( (data) => {
+            res.json( { data: data, result : 'ok'  } );
+        })
+        .catch( (err) => {
+            res.status(500).json( { code: err.code || 0, message: err.message } );
+        });
+};
+
+getProductionBy['year'] = (id, start, res) => {
+
+    let ts1 = moment(start).format('YYYY-MM-DD');
+    let ts2 = moment(start).add(1, 'y').format('YYYY-MM-DD');
 
     let q = `
     select left( t1.d, 7) as date,
@@ -157,8 +153,7 @@ getProductionBy['year'] = (id, end, res) => {
 
     global.module.getData(q)
         .then( (data) => {
-            let r = { start : ts1, end : ts2, samples : data };
-            res.json( { data: r, result : 'ok'  } );
+            res.json( { data: data, result : 'ok'  } );
         })
         .catch( (err) => {
             res.status(500).json( { code: err.code || 0, message: err.message } );
